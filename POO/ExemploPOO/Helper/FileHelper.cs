0cb7f1feb0d5361e1ecb94e3f8a4cfd44516c6e5
josiasgb <josiasgb@yahoo.com.br>
2021-12-07 21:22:45 -0300

    Primeiro commit

---
+++ POO/ExemploPOO/Helper/FileHelper.cs
@@ -0,0 +1,50 @@
+using System.IO;
+
+namespace ExemploPOO.Helper
+{
+    public class FileHelper
+    {
+        public void ListaDiretorios(string caminho) 
+        {
+            var retornoCaminho = Directory.GetDirectories(caminho, "*", SearchOption.AllDirectories);
+
+            foreach(var retorno in retornoCaminho)
+            {
+                System.Console.WriteLine(retorno);
+            }
+        }
+
+        public void ListarArquivosDiretorios(string caminho)
+        {
+            var retornoArquivos = Directory.GetFiles(caminho, "*2*", SearchOption.AllDirectories);
+            foreach(var retorno in retornoArquivos)
+            {
+                System.Console.WriteLine(retorno);
+            }
+        }
+
+        internal void CriarDiretorio(Func<string, string, string> combine)
+        {
+            throw new NotImplementedException();
+        }   
+
+        public void CriarDiretorio(string caminho)
+        {
+            var retorno = Directory.CreateDirectory(caminho);
+            System.Console.WriteLine(retorno.FullName);
+        }
+
+        public void ApagarDiretorio(string caminho, bool apagarArquivos)
+        {
+            Directory.Delete(caminho, apagarArquivos);
+        }
+
+        public void CriarArquivoTexto(string caminho, string conteudo)
+        {
+            if(!File.Exists(caminho))
+            {
+            File.WriteAllText(caminho, conteudo);
+            }
+        }
+    }
+}
---
+++ POO/ExemploPOO/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using ExemploPOO.Helper;
+using ExemploPOO.Interfaces;
+using ExemploPOO.Models;
+
+namespace ExemploPOO{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var caminho = "C:\\DIO\\TrabalhandoComArquivos";
+            var caminhoPathCombine = Path.Combine(caminho, "Pasta teste 1");
+            var caminhoArquivo = Path.Combine(caminho, "arquivo-teste.txt");
+
+            FileHelper helper  = new FileHelper();
+            //helper.ListaDiretorios(caminho);
+            //helper.ListarArquivosDiretorios(caminho);
+            //System.Console.WriteLine("Criando diretório: " + caminhoPathCombine);
+            //helper.CriarDiretorio(caminhoPathCombine);
+            //helper.ApagarDiretorio(caminhoPathCombine, true);
+            helper.CriarArquivoTexto(caminhoArquivo, "Olá! Teste de escrita de arquivo");
+
+
+
+            // ICalculadora calc = new Calculadora();
+            // System.Console.WriteLine(calc.Somar(10, 20));
+            
+            
+            // Computador comp = new Computador();
+            // System.Console.WriteLine(comp.ToString());
+
+            // Corrente c = new Corrente();
+            // c.Creditar(100);
+            // c.ExibirSaldo();
+
+            // Calculadora calc = new Calculadora();
+            // System.Console.WriteLine("Resultado da primeira soma: " + calc.Somar(10, 10));
+            // System.Console.WriteLine("Resultado da segunda soma: " + calc.Somar(10, 10, 10));
+
+            // Aluno p1 = new Aluno();
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+            // p1.Documento = "123456";
+            // p1.Nota = 10;
+            // p1.Apresentar();
+
+            // Professor p2 = new Professor();
+            // p2.Nome = "Leo";
+            // p2.Idade = 20;
+            // p2.Documento = "123456";
+            // p2.Salario = 1000;
+            // p2.Apresentar();
+
+            // //valores válidos
+            // Retangulo r = new Retangulo();
+            // r.DefinirMedidas(30, 30);
+            // System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            // //valores inválidos
+            // Retangulo r2 = new Retangulo();
+            // r2.DefinirMedidas(0, 0);
+            // System.Console.WriteLine($"Área: {r2.ObterArea()}");
+
+            // Pessoa p1 = new Pessoa();
+            // p1.Nome = "Bob";
+            // p1.Idade = 20;
+            // p1.Apresentar();
+        }
+
+    }
+}
+
